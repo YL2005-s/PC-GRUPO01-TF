65c1ca376ef5385add7c7416015beb8ff8962adf
Dani <118644371+DanielaRV04@users.noreply.github.com>
2025-11-27 02:09:53 -0500

    Update aho_corasick.hpp

--- dna-cpp/src/aho_corasick.hpp
+++ dna-cpp/src/aho_corasick.hpp
@@ -1,1 +1,10 @@
+#ifndef AHO_CORASICK_HPP
+#define AHO_CORASICK_HPP
 
+#include <string>
+#include <vector>
+#include <map>
+
+std::vector<int> AhoCorasickSearch(const std::string& text, const std::string& pattern);
+
+#endif
